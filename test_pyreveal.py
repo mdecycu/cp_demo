7f963fa24a0ab21e3b2cf75af005c141bbddc398
Zhang Zhuoyi <an552032627@gmail.com>
2025-10-01 09:45:07 +0800

    更新簡報檔案與程式碼，刪除 Group2_work 資料夾

--- test_pyreveal.py
+++ test_pyreveal.py
@@ -7,7 +7,8 @@ presentation = PyReveal(title="My Presentation", theme="white", transition="slid
 bg_image = ImageBackground(image_url=bg_image_path)
 
 # 加入幻燈片
-presentation.add_slide("Welcome!", background=bg_image)
+presentation.add_slide("計算機程式概論\n\
+可攜式系統組成內容介紹", background=bg_image)
 presentation.add_slide("Another slide", background=bg_image)
 
 # 生成 HTML 簡報
